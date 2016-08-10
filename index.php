abb3c863434c65992f187d29bf6a4e8cd3cbd343
Steve Guidetti <sguidetti@ultramegasoft.com>
2016-08-10 13:03:46 -0700

    Add language attribute

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 -->
-<html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="style.css">
